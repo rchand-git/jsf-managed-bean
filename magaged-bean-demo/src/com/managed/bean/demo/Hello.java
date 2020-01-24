a5e5676a27c7387c2fbb4899c416f61944d562f6
rchand-git <59119556+rchand-git@users.noreply.github.com>
2020-01-23 21:01:49 -0500

    jsf-managed-bean
    jsf-managed-bean

---
+++ magaged-bean-demo/src/com/managed/bean/demo/Hello.java
@@ -0,0 +1,22 @@
+package com.managed.bean.demo;
+
+public class Hello {
+
+	
+	// Java class - properities/attributes/getter/setter
+	// wtite business logic
+	// Framework will take care of bean
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
